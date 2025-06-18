d64e5ca75197109d219beee84df8ed61b941e849
Vikaramjit Goraya <vikaramjitsinghgorraya@gmail.com>
2025-06-18 05:58:32 -0400

    Added croos origin policy url

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,11 @@ dotenv.config();
 const app = express();
 
 const port = process.env.PORT || 4000;
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+  })
+);
 
 app.use(express.json());
 app.use("/api", pdfRouter);
